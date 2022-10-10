a414df18ca42960fceadb5016f588d493006e541
juebrauer <juebrauer@gmail.com>
2022-10-10 08:22:02 +0200

    Started with a first very simple program in C.

---
+++ pro1/01_kapitalberechnung.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    float kapital;
+    printf("Dein Startkapital:");
+    scanf("%f", &kapital);
+
+    printf("\nDein Startkapital ist also %f", kapital);
+
+
+    return 0;
+}

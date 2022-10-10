7a50e86dcdee6feabddae042e6ebe572f71c2c65
juebrauer <juebrauer@gmail.com>
2022-10-10 09:32:52 +0200

    Showed a first loop.

--- pro1/01_kapitalberechnung.c
+++ pro1/01_kapitalberechnung.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
+// Das ist ein kleines Programm,
+// in dem wir die ersten C-Schritte lernen
+
+
 int main()
 {
-    printf("Hello world!\n");
+    float startkapital;
+    printf("Dein Startkapital: ");
+    scanf("%f", &startkapital);
+
+    float zielkapital;
+    printf("Dein Zielkapital: ");
+    scanf("%f", &zielkapital);
+
+    float zinsrate;
+    printf("Dein Zinssatz: ");
+    scanf("%f", &zinsrate);
+
+    int jahr;
+    jahr = 0;
 
     float kapital;
-    printf("Dein Startkapital:");
-    scanf("%f", &kapital);
-
-    printf("\nDein Startkapital ist also %f", kapital);
-
+    kapital = startkapital;
+    while (kapital<zielkapital)
+    {
+        kapital = kapital * (1+zinsrate/100);
+        jahr = jahr + 1;
+        printf("\nNach %d Jahr hast du: %f", jahr, kapital);
+    }
 
     return 0;
+    
 }

937dc61a9ad1792f9b6d398379655aa0915dab42
juebrauer <juebrauer@gmail.com>
2022-10-13 12:03:53 +0200

    Introduced the do-while-loop as a further example of a loop, where a block of commands is executed at least once.

--- pro1/01_kapitalberechnung.c
+++ pro1/01_kapitalberechnung.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
-
-// Das ist ein kleines Programm,
-// in dem wir die ersten C-Schritte lernen
-
-
 int main()
-{
-    float startkapital;
-    printf("Dein Startkapital: ");
-    scanf("%f", &startkapital);
-
-    float zielkapital;
-    printf("Dein Zielkapital: ");
-    scanf("%f", &zielkapital);
-
-    float zinsrate;
-    printf("Dein Zinssatz: ");
-    scanf("%f", &zinsrate);
-
-    int jahr;
-    jahr = 0;
-
-    float kapital;
-    kapital = startkapital;
-    while (kapital<zielkapital)
+{       
+    char weiter;
+    do
     {
-        kapital = kapital * (1+zinsrate/100);
-        jahr = jahr + 1;
-        printf("\nNach %d Jahr hast du: %f", jahr, kapital);
-    }
+        float startkapital;
+        
+        printf("\nDein Startkapital: ");
+        scanf("%f", &startkapital);
+
+        float zielkapital;
+        printf("Dein Zielkapital: ");
+        scanf("%f", &zielkapital);
+
+        float zinsrate;
+        printf("Dein Zinssatz: ");
+        scanf("%f", &zinsrate);
+
+        int jahr;
+        jahr = 0; 
+
+        float kapital;
+        kapital = startkapital;
+        while (kapital<zielkapital)
+        {
+            kapital = kapital * (1+zinsrate/100);
+            jahr = jahr + 1;
+            printf("\nNach %d Jahr hast du: %f", jahr, kapital);
+        }
+
+        printf("\nWillst du eine weitere Kapitalverzinsung"
+            "berechnen? (j/n)");
+       
+        
+        scanf(" %c", &weiter);
+        printf("Du hast folgendes Zeichen eingegeben: %c", weiter);
+
+            
+
+    } while (weiter=='j');
 
     return 0;
-    
 }
+
+
